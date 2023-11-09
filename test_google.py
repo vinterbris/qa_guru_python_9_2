f74556eb6a52e6b74c7d68267d352acf0bc50c9d
Sergey <21102027+vinterbris@users.noreply.github.com>
2023-11-09 18:12:14 +0300

    fix for test_google

--- test_google.py
+++ test_google.py
@@ -9,11 +9,14 @@ def site():
     config.window_height = 900
     browser.open('https://google.com')
 
+    yield
+    browser.quit()
 
 
 def test_google_search(site):
     browser.element('[name="q"]').should(be.blank).type('yashaka/selene').press_enter()
     browser.element('[id="search"]').should(have.text('Selene - User-oriented Web UI browser tests in Python'))
+
 
 def test_google_no_result(site):
     browser.element('[name="q"]').should(be.blank).type('a;lsdkfjal;skjdfaskjdhfjklahsdfklj').press_enter()
